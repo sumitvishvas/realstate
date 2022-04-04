7c23a2303cf4810aa3af663d71893059290426da
SUMIT VISHVAS <vishvassumit333@gmail.com>
2022-04-04 16:07:26 +0530

    intial commit

--- util/db.js
+++ util/db.js
@@ -1,12 +1,12 @@
 const Sequelize  = require('sequelize');
-const sequelize=new  Sequelize('baafilac_realEstate' ,'baafilac_rajs01878','uxp[he@KZo-c',{host:'103.224.247.222',
-dialect:'mysql'
-});
-
-
-// const sequelize=new  Sequelize('baafilac_realEstate' ,'root','',{host:'localhost',
+// const sequelize=new  Sequelize('baafilac_realEstate' ,'baafilac_rajs01878','uxp[he@KZo-c',{host:'103.224.247.222',
 // dialect:'mysql'
 // });
+
+
+const sequelize=new  Sequelize('baafilac_realEstate' ,'root','',{host:'localhost',
+dialect:'mysql'
+});
 
 
 
